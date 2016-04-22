ba4d0817e6808f0f9c65c327b436628833f2a440
Ron Dagdag <rlyle78@gmail.com>
2016-04-22 18:09:45 -0500

    add video-facerecognition

--- video-facerecognition.js
+++ video-facerecognition.js
@@ -1,6 +1,5 @@
 var cv = require('opencv');
 
-
 console.log("Started Training");
 var trainingData = [];
 /*for (var i = 0; i< 243; i++){
@@ -19,47 +18,49 @@ facerec.trainSync(trainingData);
 console.log("Done Training")
 
 console.log("Recognizing...")
+face_cascade = new cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
+console.log("video")
 video_stream = new cv.VideoCapture(0);
-//# we create a window to display the Video frames
-namedWindow = new cv.NamedWindow('Video',1)
+var window = new cv.NamedWindow('Video', 0)
 
-//# We set an interval to retrieve frames from the
-//# video source and we get the intervalId so we can
-//# stop the program from the video feed window.
-
-intervalId = setInterval(()->
-    video_stream.read(function(err, im0){
+video_stream.read(function(err, im0){
     var x = 0;
     var iter = function(){
         video_stream.read(function(err, im){
         
         if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+        window.show(im);
+    //im.detectObject("haarcascade_frontalface_alt.xml", {}, function(err, faces){
     
-            im.detectObject("haarcascade_frontalface_alt.xml", {}, function(err, faces){
-                if (err) throw err;
-                
-                img_gray = im.copy();
-                img_gray.convertGrayscale();
-                
+        if (err) throw err;
+        
+        img_gray = im.copy();
+        img_gray.convertGrayscale();
+        face_cascade.detectMultiScale(img_gray,
+            function(err, faces) {
                 for (var i = 0; i < faces.length; i++){
                 var face = faces[i];
                 img_crop = img_gray.crop(face.x,face.y,face.width,face.height)
                 img_crop.resize(60,60, 1);
-                img_crop.save( + x + '.jpg');
+                //img_crop.save( + x + '.jpg');
                 console.log(facerec.predictSync(img_crop));
-                //im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
-                }
-            })
-        //   Finally we get the key pressed on the window to terminate
-        // execution of the program.
-        namedWindow.blockingWaitKey(0, 20)
-
+                im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+            }    
+        });
+        window.blockingWaitKey(0, 50);         
     
-    
-            })
+        x++;
+        if (x>100) {
+            console.log('done');
+            video_stream.readable = false;
+        }
+        else {   
+         iter();
+        }
+    })
   }
   iter();
-}),50);
+});
 
 
